fc908bb094c896e542be4d8c90cbed3408a74f49
Akshat Kotwalla <akshatkotwalla@gmail.com>
2023-07-09 00:47:24 +0530

    added assessment form and managed states along with setting post API call working

---
+++ akshat/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './component/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    
+    </div>
+  );
+}
+
+export default App;
